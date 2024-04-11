7af4f1e055b2e7af7c2c00ec5417c0f1fad4a728
Crissa Owens <crissao@udel.edu>
2024-04-11 16:29:23 -0400

    prototypes added to display.c and input.c

---
+++ display.c
@@ -0,0 +1,18 @@
+#include "scroll.h"
+
+void open_display(void){
+
+}
+
+void close_display(void){
+
+}
+
+void display_letter(char letter, int xOffset, int yOffset){
+
+}
+
+void clear_display(void){
+
+}
+
---
+++ input.c
@@ -0,0 +1,13 @@
+#include "scroll.h"
+
+void open_input(void){
+
+}
+
+void close_input(void){
+
+}
+
+void check_input(void (*callback)(unsigned int code), int delay){
+
+}
