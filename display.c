a90e93a2ff4be6f5bf269fa6a8e0741b73b0f497
Crissa Owens <crissao@udel.edu>
2024-04-13 13:16:15 -0400

    display_letter draws dot at (xOffset, yOffset)

--- display.c
+++ display.c
@@ -17,7 +17,22 @@ void close_display(void){
 }
 
 void display_letter(char letter, int xOffset, int yOffset){
-
+	clearFrameBuffer(fb,BLACK);
+	if (xOffset < 0){
+		xOffset *= -1;
+		xOffset = xOffset%8;
+		xOffset *= -1;
+	}else if (xOffset > 7){
+		xOffset = xOffset%8;
+	}
+	if (yOffset < 0){
+		yOffset *= -1;
+		yOffset = yOffset%8;
+		yOffset *= -1;
+	}else if (yOffset > 7){
+		yOffset = yOffset%8;
+	}
+	bm->pixel[xOffset][yOffset]=WHITE;
 }
 
 void clear_display(void){
