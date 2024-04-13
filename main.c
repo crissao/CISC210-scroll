036e1e3dc3d234f6f1396f447dc64c34e3929db4
Liliana Rafoth <lrafoth@udel.edu>
2024-04-13 13:19:01 -0400

    complete (?) main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "display.h"
+
+int main () {
+	open_display();
+	display_letter('a', 2, 2);
+	sleep(1);
+	display_letter('a', 11, 19);
+	sleep(1);
+	display_letter('a', -12, -5);
+	clear_display();
+	close_display();
+	}
+
