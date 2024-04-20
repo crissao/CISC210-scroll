50aae1ff484d3aa9b3f7ae964a155a3781126120
Crissa Owens <crissao@udel.edu>
2024-04-20 16:23:33 -0400

    almost working but skips spots

--- main.c
+++ main.c
@@ -4,23 +4,31 @@ void dot_roll(void);
 
 int main () {
 	open_display();
-	display_letter('c',5.5,2.2);
+
 	dot_roll();
 	}
 
 
 void dot_roll(void){
-    int vx,vy;
-    printf("Input two integer values between -10 and 10, with a space between\n");
-    scanf("%d %d",&vx,&vy);
-    int x = 0;
-    int y = 0;
-    int count = 0;
-    for(int i = 0; i>=0; i++){
-        x += vx*count;
-        y += vy*count;
-        display_letter('c',x,y);
-	count += 1;
+   	int vx,vy;
+   	printf("Input two integer values between -10 and 10, with a space between\n");
+    	scanf("%d %d",&vx,&vy);
+    	int x = 7;
+    	int y = 7;
+    	int i = 1;
+    	while(i > 0){
+		display_letter('c',x,y);	
+        	x += -vx;
+        	y += -vy;
+        	while (x < 0){
+	       		x += 8;}
+        	if (x >= 8){
+	       		x -= 8;}
+        	while (y < 0){
+	       		y += 8;}
+        	if (y >= 8) {
+			y -= 8;}   
+		sleep(1);
     }
 }
 
