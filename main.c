09e560ea5a8190f7f563a2db9062946ba469b2dd
Liliana Rafoth <lrafoth@udel.edu>
2024-04-24 21:52:55 -0400

    close to getting dot_roll (??)

--- main.c
+++ main.c
@@ -13,22 +13,24 @@ void dot_roll(void){
    	int vx,vy;
    	printf("Input two integer values between -10 and 10, with a space between\n");
     	scanf("%d %d",&vx,&vy);
-    	int x = 7;
-    	int y = 7;
-    	int i = 1;
-    	while(i > 0){
-		display_letter('c',x,y);	
-        	x += -vx;
-        	y += -vy;
-        	while (x < 0){
-	       		x += 8;}
-        	if (x >= 8){
-	       		x -= 8;}
-        	while (y < 0){
-	       		y += 8;}
-        	if (y >= 8) {
-			y -= 8;}   
+    	int x = 0;
+    	int y = 0;
+	float floatx = 0.0;
+	float floaty = 0.0;
+	float stepx = vx / 10.0;
+	float stepy = vy / 10.0;
+	while (1) {
+		if (floatx >= 1.0) {
+			stepx--;
+		}
+		if (floaty >= 1.0) {
+			stepy--;
+		}
+		x = ((x % 8) + 8) % 8;
+		y = ((y % 8) + 8) % 8;
+		floatx += stepx;
+		floaty += stepy;
+		display_letter('c', x, y);
 		sleep(1);
-    }
+	}
 }
-
