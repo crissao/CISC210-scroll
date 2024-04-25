b8347cd957734c8a95d05962151f16d7df6596eb
Crissa Owens <crissao@udel.edu>
2024-04-24 22:17:30 -0400

    okay i think it goes the right way - STAGE1 COMPLETE

--- main.c
+++ main.c
@@ -15,18 +15,18 @@ void dot_roll(void){
     	scanf("%d %d",&vx,&vy);
     	int x = 0;
     	int y = 0;
-	float floatx = 0.0;
-	float floaty = 0.0;
+	float floatx = 7.0;
+	float floaty = 7.0;
 	float stepx = vx / 10.0;
 	float stepy = vy / 10.0;
 	while (1) {
 		if (floatx >= 1.0) {
 			floatx--;
-			x++;
+			x--;
 		}
 		if (floaty >= 1.0) {
 			floaty--;
-			y++;
+			y--;
 		}
 		x = ((x % 8) + 8) % 8;
 		y = ((y % 8) + 8) % 8;
