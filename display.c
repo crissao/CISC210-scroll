48a67f375142f6a1310d23e688209620cf3b155d
Crissa Owens <crissao@udel.edu>
2024-04-27 14:47:20 -0400

    letter C added to display_letter

--- display.c
+++ display.c
@@ -29,8 +29,24 @@ void display_letter(char letter, int xOffset, int yOffset){
 		    yOffset += 8;
         }
 	}
-	bm->pixel[xOffset%8][yOffset%8]=WHITE;
-}
+	if (letter == 'C'){
+		bm->pixel[1][1]=PURPLE;
+   		bm->pixel[2][0]=BLUE;
+   		bm->pixel[3][0]=LIGHTBLUE;
+   		bm->pixel[4][0]=GREEN;
+   		bm->pixel[5][0]=YELLOW;
+   		bm->pixel[6][1]=ORANGE;
+    		bm->pixel[7][2]=RED;
+    		bm->pixel[7][3]=RED;
+   		bm->pixel[7][4]=RED;
+   		bm->pixel[7][5]=RED;
+   		bm->pixel[6][6]=ORANGE;
+   		bm->pixel[5][7]=YELLOW;
+   		bm->pixel[4][7]=GREEN;
+    		bm->pixel[3][7]=LIGHTBLUE;
+    		bm->pixel[2][7]=BLUE;
+   		bm->pixel[1][6]=PURPLE;	
+	}
 
 void clear_display(void){
 	clearFrameBuffer(fb,BLACK);
