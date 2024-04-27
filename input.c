8261b2057019310b4216c940ff6ddc4772cd2e44
Crissa Owens <crissao@udel.edu>
2024-04-27 12:31:35 -0400

    making progress on the joystick and scrolling

--- input.c
+++ input.c
@@ -1,13 +1,17 @@
 #include "scroll.h"
 
+
+pi_joystick_t* joystick;
+
 void open_input(void){
+	joystick=getJoystickDevice();
 
 }
 
 void close_input(void){
-
+	freeJoystick(joystick);
 }
 
 void check_input(void (*callback)(unsigned int code), int delay){
-
+	pollJoystick(joystick,callback,delay);
 }
--- main.c
+++ main.c
@@ -1,14 +1,67 @@
 #include "scroll.h"
 
 void dot_roll(void);
+void handler(unsigned int code);
+
+int running = 1, vx = 0, vy = 0;
+int count = 0;
+char letter = 'C';
+
 
 int main () {
 	open_display();
-
-	dot_roll();
+	open_input();
+	float floatx = 0.0;
+	float floaty = 0.0;
+	int x = 0;
+    	int y = 0;
+	while(running){
+		float stepx = vx / 10.0;
+		float stepy = vy / 10.0;
+		if (floatx >= 1.0) {
+			floatx--;
+			x--;
+		}
+		if (floaty >= 1.0) {
+			floaty--;
+			y--;
+		}
+		x = ((x % 8) + 8) % 8;
+		y = ((y % 8) + 8) % 8;
+		floatx += stepx;
+		floaty += stepy;
+		display_letter(letter, x, y);
+		check_input(handler, 1000);
+	}
+	clear_display();
+	close_input();
+	close_display();
 	}
 
+void handler (unsigned int code){
+	if(code == KEY_UP) {
+		vy++;
+    	}else if(code == KEY_DOWN) {
+		vy--;
+	}else if(code == KEY_RIGHT) {
+		vx++;
+    	}else if(code == KEY_LEFT) {
+		vx--;
+    	}else if(code == KEY_ENTER) {
+		count += 1;
+		if (count == 1){
+			letter = 'O';
+		} else if (count == 2){
+			letter = 'L';
+		} else if (count == 3){
+			letter = 'R';
+		} else if (count > 3){
+			running = 0;
+		}
+    }
+}	
 
+/*
 void dot_roll(void){
    	int vx,vy;
    	printf("Input two integer values between -10 and 10, with a space between\n");
@@ -35,4 +88,6 @@ void dot_roll(void){
 		display_letter('c', x, y);
 		usleep(100000);
 	}
+
 }
+*/
